3e4bcb3d79982d5cf160de0424ba8e8d83d52e4d
Daniel Bak <DanielPBak@gmail.com>
2020-03-06 00:36:25 -0700

    Substantially lowered probability of enormous chains. Added __main__ function for local development

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,7 @@ from urllib import request
 import json
 import io
 url = r"https://docs.google.com/spreadsheets/d/e/2PACX-1vR1woWAU1ClzNJBUElMoxLstPYmhq0JfdajTjBABMM3TqpLE5wevkO6SHeoz2a6NS0pDielm9Zx2bWB/pub?gid=0&single=true&output=csv"
+from collections import defaultdict
 
 
 class Generator:
@@ -33,6 +34,8 @@ class Generator:
     
     def __init__(self, html):
         self.html = html.decode('utf-8')
+        self.trace = []
+        self.debug_mode = False
 
     def get_words(self):
 
@@ -69,8 +72,6 @@ class Generator:
                     hasp_weight = 1
                 else:
                     hasp_weight = float(row[3])
-
-
 
                 if type == 'DESCADJ':
                     self.desc_adjs.append(words)
@@ -121,40 +122,67 @@ class Generator:
     def get_ideologies(self, n_ideologies=100):
 
         ideologies = []
+        traces = []
 
         for i in range(0, n_ideologies):
+            self.trace = []
             ideologies.append(self.generate_ideology())
-        return ideologies
+            traces.append(self.trace)
+        return ideologies, traces
 
-    def get_noun(self, p_adj=0.5, p_prefix=0.5, p_double=0.25):
+    def get_noun(self, p_adj=0.5, p_prefix=0.5, p_double=0.25, is_double=False):
 
+        self.trace.append('Noun')
         noun = random.choice(self.nouns[self.weighted_choice(self.nouns_weights)])
 
-        if random.random() < p_prefix:
-            new_p_prefix = min(p_prefix * p_prefix, p_prefix * .33)
-            prefix = self.get_prefix(new_p_prefix)
+        if self.debug_mode:
+            noun = 'noun'
+        has_double = random.random() < p_double
+
+        # If we have a double, lower the odds of prefixes and adjs.
+        if has_double:
+            p_prefix = p_prefix / 2
+            p_adj = p_adj / 2
+
+        has_prefix = random.random() < p_prefix
+        has_adj = random.random() < p_adj
+        new_p_prefix = min(p_prefix * p_prefix, p_prefix * .33)
+        new_p_adj = min(p_adj * p_adj, p_adj * .33)
+
+        if has_prefix:
+            if is_double:
+                prefix_p_hyphen = 0
+            else:
+                prefix_p_hyphen = .25
+            prefix = self.get_prefix(new_p_prefix, p_hyphen=prefix_p_hyphen)
             noun = prefix + noun
 
         noun = noun
 
-        if random.random() < p_adj:
-            new_p_adj = min(p_adj * p_adj, p_adj * .33)
-            noun = self.get_desc(new_p_adj) + ' ' + noun
+        if has_adj:
+            noun = self.get_desc(new_p_adj, new_p_prefix) + ' ' + noun
 
-        if random.random() < p_double:
-            noun = noun + '-' + self.get_noun(p_adj=0, p_prefix=0.25, p_double=0)
+        if has_double:
+            noun = noun + '-' + self.get_noun(p_adj=0, p_prefix=0.25, p_double=0, is_double=True)
 
         return noun
 
     def get_desc(self, p_prefix=0.5, p_adj=0.33, p_double=0.4, p_adverb=0.2):
+        new_p_prefix = min(p_prefix * p_prefix, p_prefix * .33)
+        new_p_adj = min(p_adj * p_adj, p_adj * .33)
 
+        self.trace.append('Desc')
         desc_idx = self.weighted_choice(self.all_descs_weights)
 
         adjs = self.all_descs[desc_idx]
 
         add_adverb = False
+        if adjs in self.desc_adjs:
+            is_desc_adj = True
+        else:
+            is_desc_adj = False
 
-        if adjs in self.desc_adjs and random.random() < p_adverb:
+        if is_desc_adj and random.random() < p_adverb:
             add_adverb = True
 
         adj = random.choice(adjs)
@@ -168,31 +196,40 @@ class Generator:
         adj = adj.replace('{Noun}', '{noun}')
 
         if '{adj1}' in adj or '{adj2}' in adj:
-            adj = adj.format(adj1=self.get_desc(p_adj=p_adj * p_adj * p_adj, p_double=0),
-                             adj2=self.get_desc(p_adj=p_adj * p_adj * p_adj, p_double=0))
+            adj = adj.format(adj1=self.get_desc(p_adj=new_p_adj, p_prefix=new_p_prefix, p_double=0),
+                             adj2=self.get_desc(p_adj=new_p_adj, p_prefix=new_p_prefix, p_double=0))
 
             return adj
 
+        if self.debug_mode:
+            adj = 'adj'
 
-
-        if random.random() < p_double:
+        if is_desc_adj and random.random() < p_double:
             adj = adj + '-' + self.get_desc(p_adj=0, p_double=0, p_prefix=0.1, p_adverb=0)
 
         if random.random() < p_prefix * prefix_weight:
-            adj = self.get_prefix() + adj
+            adj = self.get_prefix(p_prefix=new_p_prefix) + adj
 
         if add_adverb:
             adj = self.get_adverb() + ' ' + adj
 
         if random.random() < p_adj:
-            adj = '{} {}'.format(self.get_desc(p_prefix * .8, p_adj * p_adj * p_adj), adj)
+            adj = '{} {}'.format(self.get_desc(new_p_prefix, new_p_adj), adj)
+
         return adj
 
     def get_prefix(self, p_prefix=0.15, p_hyphen=0.5):
+        self.trace.append('prefix')
+        new_p_prefix = min(p_prefix * p_prefix, p_prefix * .33)
+
         prefix_idx = self.weighted_choice(self.prefix_weights)
         prefix_weight = self.prefix_hasp_weights[prefix_idx]
 
         pref = random.choice(self.prefixes[prefix_idx])
+
+        if self.debug_mode:
+            pref = 'pref'
+
 
         if pref.endswith('-'):
             p_next_hyphen = 1
@@ -204,32 +241,43 @@ class Generator:
                 p_next_hyphen = 1
 
         if random.random() < p_prefix * prefix_weight:
-            pref = self.get_prefix(p_hyphen=p_next_hyphen, p_prefix=p_prefix*.6) + pref
+            pref = self.get_prefix(p_hyphen=p_next_hyphen, p_prefix=new_p_prefix) + pref
 
         return pref
 
     def get_ending(self):
+        self.trace.append('ending')
+        ending = random.choice(self.endings[self.weighted_choice(self.endings_weights)]).title()
 
-        ending = random.choice(self.endings[self.weighted_choice(self.endings_weights)])
 
         ending = ending.replace('{Adj}', '{adj}')
         ending = ending.replace('{Noun}', '{noun}')
-        ending = ending.format(adj=self.get_desc(p_double=0.5),
-                               noun=self.get_noun(p_adj=0.1))
+
+        desc = self.get_desc(p_double=0.2, p_prefix=0.2, p_adj=0.1)
+        noun = self.get_noun(p_adj=0.1, p_prefix=0.2, p_double=0.2)
+
+        ending = ending.format(adj=desc,
+                               noun=noun)
 
 
         return ending
 
     def get_adverb(self):
+        self.trace.append('adverb')
         # adverb can have adj?
+
 
         adverb = random.choice(self.adverbs[self.weighted_choice(self.adverbs_weights)])
 
+        if self.debug_mode:
+            adverb = 'adverb'
         return adverb
 
 
     def generate_ideology(self, p_ending=0.33, p_start=0.5):
         force_prefix = random.choice([False, True])
+        with_ending = random.random() < p_ending
+
 
         if force_prefix:
             p_prefix = 1
@@ -237,13 +285,19 @@ class Generator:
         else:
             p_prefix = .75
             p_adj = 1
-        ideo = '{}'.format(self.get_noun(p_prefix=p_prefix, p_adj=p_adj))
 
-        with_ending = random.random() < p_ending
+        # if with_ending:
+        #     p_prefix = p_prefix / 2
+        #     p_adj = p_adj / 2
+
+        noun = self.get_noun(p_prefix, p_adj)
+
+        ideo = '{}'.format(noun)
 
         if with_ending:
 
             ending = self.get_ending()
+
 
             ending = ending.strip()
 
@@ -252,7 +306,7 @@ class Generator:
             else:
                 ending = ' ' + ending
 
-            ideo = ideo + '{}'.format(ending).title()
+            ideo = ideo + '{}'.format(ending)
 
             # characteristics = self.get_desc(p_double=.40)
             #
@@ -268,7 +322,7 @@ gen.get_words()
 
 def generate_one_ideo():
 
-    ideologies = gen.get_ideologies(1)
+    ideologies, traces = gen.get_ideologies(1)
 
     return ideologies[0]
                 
@@ -284,3 +338,32 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps(ideology)
     }
+
+
+if __name__ == '__main__':
+    n_ideologies = 10000
+    ideologies, traces = gen.get_ideologies(n_ideologies)
+
+    counts = defaultdict(int)
+    lens = []
+    for idx, ideo in enumerate(ideologies):
+        trace = traces[idx]
+
+        counts[len(trace)] += 1
+
+        if len(traces[idx]) != 0:
+            print(ideo)
+            # print(ideo)
+            # print(traces[idx])
+        lens.append(len(traces[idx]))
+        # print(ideo)
+
+    print(sum(lens) / len(lens))
+
+    for k in counts:
+        counts[k] = counts[k] / n_ideologies
+
+    s = sorted(counts.items())
+
+    for k in s:
+        print(k)
