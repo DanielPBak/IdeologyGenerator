1f444db6cccd4eeccdd9c55071b92c74e959a9f3
DanielPBak <DanielPBak@gmail.com>
2020-10-31 05:00:24 -0600

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -334,7 +334,7 @@ def lambda_handler(event, context):
             client_id = body['g_client_id']
         else:
             n_ideologies = 5
-        if random.random() < 0.01 and True:
+        if random.random() < 1 and True:
             ideologies = ["I know who you are.", "I know what you're doing.", "I know where you live.", "Prepare.", "Prepare." "JK, just a Halloween prank!"]
         else:
             ideologies = gen.get_ideologies(n_ideologies)[0]
