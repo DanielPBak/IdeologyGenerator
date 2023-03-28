c0f76bc0ff6093d632c15587aea0c8fe27391551
DanielPBak <DanielPBak@gmail.com>
2023-03-28 15:17:24 -0700

    Dumping a dict rather than just a list of ideologies so that we can handle more return possibilities.

--- lambda_function.py
+++ lambda_function.py
@@ -357,7 +357,9 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps(ideologies)
+        'body': {
+            'ideologies': json.dumps(ideologies)
+        }
     }
 
 
